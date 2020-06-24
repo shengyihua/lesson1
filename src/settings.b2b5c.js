655d5cb3a1a6e677b43ed0ecb76cf4835a46d77e
益华 <shengyihua88@gmail.com>
2020-06-24 23:19:25 +0800

    Add files via upload

---
+++ src/settings.b2b5c.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/home.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"64905",resources:"6465b",main:"8ded5"}};
